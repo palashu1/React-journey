d3e510fc195363b7b6cd8a86913f2d7dbef907fa
Ashutosh Pal <Ashutoshpal3179@gmail.com>
2026-04-28 18:22:52 +0530

    add 02-components folder

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Ashutosh Pal</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, deserunt.
+      </div>
+       <div className='card'>
+        <h1>Ashutosh Pal</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, deserunt.
+      </div>
+    </div>
+  )
+}
+
+export default App
