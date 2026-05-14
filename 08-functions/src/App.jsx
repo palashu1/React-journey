8f8fdbdd97747ae4aecbac2b8ad3922db7556a80
Ashutosh Pal <Ashutoshpal3179@gmail.com>
2026-05-14 09:28:52 +0530

    modify app.jsx and index.css file

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 const App = () => {
+  const btnClickCount = () => {
+    console.log("Button clicked");
+  };
   return (
     <div>
-      <div
-        onMouseMove={(elem) => {
-          console.log(elem.clientX);
-        }}
-        className="box"
-      ></div>
+      <button onClick={btnClickCount}>Click Here</button>
     </div>
   );
 };
